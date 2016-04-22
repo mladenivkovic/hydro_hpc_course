492a018a4f3db8753417c246ee3a37ffe3815dcc
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-04-22 13:37:21 +0200

    initial commit

---
+++ Py/read.py
@@ -0,0 +1,22 @@
+import fortranfile
+import numpy
+from matplotlib import pyplot
+
+map_file = '/scratch/daint/biernack/hpc1b/jet/output_00010.00000'
+
+# read image data
+f = fortranfile.FortranFile(map_file)
+[t, gamma] = f.readReals()
+[nx,ny,nvar,nstep] = f.readInts()
+dat = f.readReals()
+f.close()
+
+dat = numpy.array(dat)
+dat = dat.reshape(nvar,ny,nx)
+
+fig = pyplot.figure()
+ax = fig.add_subplot(1,1,1)
+
+ax.imshow(dat[0,:,:],interpolation='nearest')
+
+pyplot.show()
