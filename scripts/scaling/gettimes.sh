6ca29f598d77f16ff4dedd17737754328dcca56d
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-06-02 16:44:11 +0200

    minor update from dora

--- scripts/scaling/gettimes.sh
+++ scripts/scaling/gettimes.sh
@@ -1,19 +1,22 @@
-!#/bin/bash
+#!/bin/bash
 
 wd=$PWD
 printf "%s\t%s\t%s\n" "#ncpu" "cpu-time" "     real-time" > $wd/times.dat
 
-for i in 1 2 4 8 16 32 128 256; do
+for i in {1..256}; do
 
-    cd $i
-    if [ -f run*.log ]; then
-        echo "reading from " $i
-        cputime=`tail -n 2 run*.log | awk 'NR==1{print $5}'`
-        realtime=`tail -n 1 run*.log | awk '{print $5}'`
-        printf "%s\t%s\t%s\n" "$i" "$cputime" "$realtime" >> $wd/times.dat
-    else
-        echo $i": no runlog found."
+    if [ -d "$i" ] ; then
+        cd $i
+        if [ -f run*.log ]; then
+            echo "reading from " $i
+            cputime=`tail -n 2 run*.log | awk 'NR==1{print $5}'`
+            realtime=`tail -n 1 run*.log | awk '{print $5}'`
+            printf "%s\t%s\t%s\n" "$i" "$cputime" "$realtime" >> $wd/times.dat
+        else
+            echo $i": no runlog found."
+        fi
+        cd ..
+
     fi
-    cd ..
 
 done
