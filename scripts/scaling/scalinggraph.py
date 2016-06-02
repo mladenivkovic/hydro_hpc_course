a207c1a01fba32c4e7995b0f188fcd77dced75ac
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-06-02 16:39:12 +0200

    minor changes. still working on it.

--- scripts/scaling/scalinggraph.py
+++ scripts/scaling/scalinggraph.py
@@ -13,22 +13,22 @@ import matplotlib.pyplot as plt
 
 
 
-outputfilename = "weak_scaling_plot"
-title='Weak Scaling'
+outputfilename = "scaling_plot"
+title='Hydro Code Scaling'
 workdir= str(getcwd())
 
 
 
-def extract_ascii_times(filename):
+def get_times(filename):
 
     print " Extracting data from file"
 
     data=np.loadtxt(filename)
     nproc=data[:,0]
-    cputime=data[:,1]
-    elapsedtime=data[:,2]
+    #cputime=data[:,1]
+    time=data[:,2]
 
-    return nproc,cputime,elapsedtime
+    return nproc,time
 
 
 
@@ -43,35 +43,41 @@ if __name__ == "__main__":
 
     print "Creating figure"
 
-    nproc, cputime,elapsedtime = extract_ascii_times('times.dat')
+    nls, tls= get_times('times-linear-strong.dat')
+    nlw, tlw= get_times('times-linear-weak.dat')
+    nss, tss= get_times('times-square-strong.dat')
+    nsw, tsw= get_times('times-square-weak.dat')
 
-    
+    stls=tls[0]
+    stlw=tlw[0]
+    stss=tss[0]
+    stsw=tsw[0]
+   
     # creating empty figure with 3 subplots
-    fig = plt.figure(facecolor='white', figsize=(10,4))
-    fig.suptitle(title, family='serif', size=20) 
-    ax1 = fig.add_subplot(221)#, aspect='equal', clip_on=True)
+    fig = plt.figure(facecolor='white')#, figsize=(10,4))
+    fig.suptitle(title, family='serif')#, size=20) 
     #ax1.set_xbound(lower=0.0,upper=1.0)
     #plt.tight_layout()
 
-    serialtime_cpu=cputime[0]
-    serialtime_elapsed=elapsedtime[0]
-    ax1.set_xlim([0.0, nproc[len(nproc)-1]])
-    ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1 )
-    ax1.plot(nproc, serialtime_cpu/cputime, linewidth=0.1)
-    ax1.plot(nproc, serialtime_cpu/cputime, 'ro', markersize=1.5, markeredgecolor='r')
-    ax1.set_title("CPU time")
+
+
+    ax1 = plt.subplot(121)#, aspect='equal')
+    ax1.set_xlim([0.0, nls[-1]])
+    ax1.grid(color='lightgrey',linestyle='dashed', linewidth=0.1 )
+    ax1.plot(nls, stls/tls, 'r-', linewidth=0.1, label='linear domain')
+    ax1.plot(nls, stls/tls, 'ro', markersize=1.5, markeredgecolor='r')
+    ax1.legend(loc=0)
+    ax1.plot(nss, stss/tss, 'ro', markersize=1.5, markeredgecolor='r')
+    ax1.plot(nss, stss/tss, 'r-', linewidth=0.1, label='square domain')
+    ax1.legend(loc=0)
+    ax1.set_title("Strong Scaling")
 
 
     
-    ax2 = fig.add_subplot(222)#, aspect='equal')
-    ax2.set_xlim([0.0, nproc[len(nproc)-1]])
-    ax2.grid(color='lightgrey',linestyle='solid', linewidth=0.1 )
-    ax2.plot(nproc, serialtime_elapsed/elapsedtime, linewidth=0.1)
-    ax2.plot(nproc, serialtime_cpu/cputime, 'ro', markersize=1.5, markeredgecolor='r')
-    ax2.set_title("elapsed time")
     print "Figure created"
-    
-    
+
+#    plt.subplot_tool()
+#    plt.show() 
     # saving figure
     fig_path = workdir+'/'+outputfilename+'.pdf'
     print "saving figure as"+fig_path
