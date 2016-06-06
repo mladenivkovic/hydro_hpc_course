e11c400bbc40dd70af69a2cdc56e17add16848d6
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-06-06 15:05:17 +0200

    works as I want it to

--- scripts/scaling/scalinggraph.py
+++ scripts/scaling/scalinggraph.py
@@ -76,15 +76,15 @@ if __name__ == "__main__":
 
     ax1 = plt.subplot(121)#, aspect='equal')
     ax1.set_xlim([0.0, nsw[-1]+1])
-    ax1.plot(nlw, tlw, 'r-', linewidth=0.1, label='linear domain', zorder=5)
-    ax1.plot(nlw, tlw, 'ro', markersize=2.5, markeredgecolor='r', zorder=5)
-    ax1.plot(nsw, tsw, 'b-', linewidth=0.1, label='square domain', zorder=5)
-    ax1.plot(nsw, tsw, 'bo', markersize=2.5, markeredgecolor='b', zorder=5)
+    ax1.plot(nlw, tlw, 'r-', linewidth=0.1, label='linear domain', zorder=5) #lines linear
+    ax1.plot(nlw, tlw, 'ro', markersize=2.5, markeredgecolor='r', zorder=5) # points linear
+    ax1.plot(nsw, tsw, 'b-', linewidth=0.1, label='square domain', zorder=5) # lines square
+    ax1.plot(nsw, tsw, 'bo', markersize=2.5, markeredgecolor='b', zorder=5) #points square
     ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1,zorder=1)
     ax1.legend(loc=0, prop={'size':9,'family':'serif'})
     ax1.set_title("Computation Time", size=13, family='serif')
     ax1.set_xlabel("P", family='serif')
-    ax1.set_ylabel(r"$t$ $(s)$", family='serif')
+    ax1.set_ylabel(r"$t_{P}$  $(s)$", family='serif')
 
 
     ax2 = plt.subplot(122)#, aspect='equal')
@@ -105,7 +105,7 @@ if __name__ == "__main__":
 
     fig_path = workdir+'/'+'weak_scaling'+'.pdf'
     print "saving figure as"+fig_path
-    plt.savefig(fig_path, format='pdf', facecolor=fig1.get_facecolor(), transparent=False, dpi=150)
+    plt.savefig(fig_path, format='pdf', facecolor=fig2.get_facecolor(), transparent=False, dpi=150)
     plt.close()
 
 
@@ -122,22 +122,22 @@ if __name__ == "__main__":
 
 
 
-    fig2 = plt.figure(2, facecolor='white')
+    fig2 = plt.figure(2, facecolor='white', figsize=(12,6))
     
-    fig1.suptitle('Strong Scaling', family='serif', size=16) 
+    fig2.suptitle('Strong Scaling', family='serif', size=16) 
     
     
     ax1 = plt.subplot(121)#, aspect='equal')
-    ax1.set_xlim([0.0, nls[-1]+1])
-    ax1.plot(nlw, lw, 'r-', linewidth=0.1, label='linear domain')
-    ax1.plot(nlw, lw, 'ro', markersize=2.5, markeredgecolor='r')
-    ax1.plot(nsw, sw, 'b-', linewidth=0.1, label='square domain')
-    ax1.plot(nsw, sw, 'bo', markersize=2.5, markeredgecolor='b')
-    ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1 )
+    ax1.set_xlim([0.0, nss[-1]+1])
+    ax1.semilogy(nls, tls, 'r-', linewidth=0.1, label='linear domain', zorder=5) # lines linear
+    ax1.semilogy(nls, tls, 'ro', markersize=2.5, markeredgecolor='r', zorder=5) # points linear
+    ax1.semilogy(nss, tss, 'b-', linewidth=0.1, label='square domain', zorder=5) # lines square
+    ax1.semilogy(nss, tss, 'bo', markersize=2.5, markeredgecolor='b', zorder=5) # point square
+    ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1 , zorder=1)
     ax1.legend(loc=0, prop={'size':9,'family':'serif'})
-    ax1.set_title("Weak Scaling", size=13, family='serif')
+    ax1.set_title("Computation Time", size=13, family='serif')
     ax1.set_xlabel("P", family='serif')
-    ax1.set_ylabel(r"$t_{mono}$/$(P \cdot t_{P})$", family='serif')
+    ax1.set_ylabel(r"$t_{P}$  $(s)$", family='serif')
   
     
     
@@ -145,24 +145,21 @@ if __name__ == "__main__":
     
     
     ax2 = plt.subplot(122)#, aspect='equal')
-    ax2.set_xlim([0.0, nls[-1]+1])
-    ax2.plot(nls, mtls/tls, 'r-', linewidth=0.1, label='linear domain')
-    ax2.plot(nls, mtls/tls, 'ro', markersize=2.5, markeredgecolor='r')
-    ax2.plot(nss, mtss/tss, 'b-', linewidth=0.1, label='square domain')
-    ax2.plot(nss, mtss/tss, 'bo', markersize=2.5, markeredgecolor='b')
-    ax2.grid(color='lightgrey',linestyle='solid', linewidth=0.1 )
+    ax2.set_xlim([0.0, nss[-1]+1])
+    ax2.plot(nls, mtls/tls, 'r-', linewidth=0.1, label='linear domain', zorder=5)
+    ax2.plot(nls, mtls/tls, 'ro', markersize=2.5, markeredgecolor='r', zorder=5)
+    ax2.plot(nss, mtss/tss, 'b-', linewidth=0.1, label='square domain', zorder=5)
+    ax2.plot(nss, mtss/tss, 'bo', markersize=2.5, markeredgecolor='b', zorder=5)
+    ax2.grid(color='lightgrey',linestyle='solid', linewidth=0.1 , zorder=1)
     ax2.legend(loc=0, prop={'size':9,'family':'serif'})
     ax2.set_title("Strong Scaling", size=13, family='serif')
     ax2.set_xlabel("P", family='serif')
     ax2.set_ylabel(r"$t_{mono}$/$t_{P}$", family='serif')
 
-    plt.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.1,wspace=0.3)
     
+    plt.subplots_adjust(left=0.08, right=0.95, top=0.85, bottom=0.1,wspace=0.3)
 
-#    plt.subplot_tool()
-#    plt.show() 
     # saving figures
-    #fig_path = workdir+'/'+outputfilename+'.pdf'
     fig_path = workdir+'/'+'strong_scaling'+'.pdf'
     print "saving figure as"+fig_path
     plt.savefig(fig_path, format='pdf', facecolor=fig2.get_facecolor(), transparent=False, dpi=150)
