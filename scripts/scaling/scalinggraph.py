99d7e4751164b841077e5473d7ba274a45940021
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-06-07 11:22:37 +0200

    update

--- scripts/scaling/scalinggraph.py
+++ scripts/scaling/scalinggraph.py
@@ -54,9 +54,6 @@ if __name__ == "__main__":
 #### WEAK SCALING
 ###########################
 
-    fig1 = plt.figure(1, facecolor='white', figsize=(12,6))
-    # enables switching between figures
-    fig1.suptitle('Weak Scaling', family='serif', size=16) 
 
     #calculating weak scaling points
     lw=[] # linear weak
@@ -67,34 +64,37 @@ if __name__ == "__main__":
     for j in range(0,len(nsw)):
         sw.append(tsw[j]/(nsw[j]*mtsw))
 
+    fig1 = plt.figure(1, facecolor='white', figsize=(12,3))
+    # enables switching between figures
+#    fig1.suptitle('Weak Scaling', family='serif', size=16) 
     ax1 = plt.subplot(121)#, aspect='equal')
     ax1.set_xlim([0.0, nsw[-1]+1])
-    ax1.plot(nlw, tlw, 'r-', linewidth=0.1, label='linear domain', zorder=5) #lines linear
+    ax1.plot(nlw, tlw, 'r-', linewidth=0.1, label='linear execution', zorder=5) #lines linear
     ax1.plot(nlw, tlw, 'ro', markersize=2.5, markeredgecolor='r', zorder=5) # points linear
-    ax1.plot(nsw, tsw, 'b-', linewidth=0.1, label='square domain', zorder=5) # lines square
+    ax1.plot(nsw, tsw, 'b-', linewidth=0.1, label='square execution', zorder=5) # lines square
     ax1.plot(nsw, tsw, 'bo', markersize=2.5, markeredgecolor='b', zorder=5) #points square
     ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1,zorder=1)
     ax1.legend(loc=0, prop={'size':9,'family':'serif'})
     ax1.set_title("Computation Time", size=13, family='serif')
-    ax1.set_xlabel("P [1]", family='serif')
+    ax1.set_xlabel("$P$  $[1]$", family='serif')
     ax1.set_ylabel(r"$t_{P}$  $[s]$", family='serif')
 
 
     ax2 = plt.subplot(122)
     ax2.set_xlim([0.0, nsw[-1]+1])
-    ax2.semilogy(nlw, lw, 'r-', linewidth=0.1, label='linear domain', zorder=5)
+    ax2.semilogy(nlw, lw, 'r-', linewidth=0.1, label='linear execution', zorder=5)
     ax2.semilogy(nlw, lw, 'ro', markersize=2.5, markeredgecolor='r', zorder=5)
-    ax2.semilogy(nsw, sw, 'b-', linewidth=0.1, label='square domain', zorder=5)
+    ax2.semilogy(nsw, sw, 'b-', linewidth=0.1, label='square execution', zorder=5)
     ax2.semilogy(nsw, sw, 'bo', markersize=2.5, markeredgecolor='b', zorder=5)
     ax2.grid(color='lightgrey',linestyle='solid', linewidth=0.1, zorder=1 )
     ax2.legend(loc=0, prop={'size':9,'family':'serif'})
     ax2.set_title("Efficiency", size=13, family='serif')
-    ax2.set_xlabel("P [1]", family='serif')
-    ax2.set_ylabel(r"$t_{1}$ / $t_{P}$ [1]", family='serif')
+    ax2.set_xlabel("$P$  $[1]$", family='serif')
+    ax2.set_ylabel(r"$t_{1}$ / $t_{P}$  $[1]$", family='serif')
 
 
 
-    plt.subplots_adjust(left=0.08, right=0.95, top=0.85, bottom=0.1,wspace=0.2)
+    plt.subplots_adjust(left=0.07, right=0.97, top=0.9, bottom=0.2,wspace=0.2)
 
     fig_path = workdir+'/'+'weak_scaling'+'.pdf'
     print "saving figure as"+fig_path
@@ -115,21 +115,21 @@ if __name__ == "__main__":
 
 
 
-    fig2 = plt.figure(2, facecolor='white', figsize=(12,6))
+    fig2 = plt.figure(2, facecolor='white', figsize=(12,3))
     
-    fig2.suptitle('Strong Scaling', family='serif', size=16) 
+    #fig2.suptitle('Strong Scaling', family='serif', size=16) 
     
     
     ax1 = plt.subplot(121)#, aspect='equal')
     ax1.set_xlim([0.0, nss[-1]+1])
-    ax1.semilogy(nls, tls, 'r-', linewidth=0.1, label='linear domain', zorder=5) # lines linear
+    ax1.semilogy(nls, tls, 'r-', linewidth=0.1, label='linear execution', zorder=5) # lines linear
     ax1.semilogy(nls, tls, 'ro', markersize=2.5, markeredgecolor='r', zorder=5) # points linear
-    ax1.semilogy(nss, tss, 'b-', linewidth=0.1, label='square domain', zorder=5) # lines square
+    ax1.semilogy(nss, tss, 'b-', linewidth=0.1, label='square execution', zorder=5) # lines square
     ax1.semilogy(nss, tss, 'bo', markersize=2.5, markeredgecolor='b', zorder=5) # point square
     ax1.grid(color='lightgrey',linestyle='solid', linewidth=0.1 , zorder=1)
     ax1.legend(loc=0, prop={'size':9,'family':'serif'})
     ax1.set_title("Computation Time", size=13, family='serif')
-    ax1.set_xlabel("P [1]", family='serif')
+    ax1.set_xlabel("$P$  $[1]$", family='serif')
     ax1.set_ylabel(r"$t_{P}$  $[s]$", family='serif')
   
     
@@ -139,19 +139,19 @@ if __name__ == "__main__":
     
     ax2 = plt.subplot(122)
     ax2.set_xlim([0.0, nss[-1]+1])
-    ax2.plot(nls, mtls/tls, 'r-', linewidth=0.1, label='linear domain', zorder=5)
+    ax2.plot(nls, mtls/tls, 'r-', linewidth=0.1, label='linear execution', zorder=5)
     ax2.plot(nls, mtls/tls, 'ro', markersize=2.5, markeredgecolor='r', zorder=5)
-    ax2.plot(nss, mtss/tss, 'b-', linewidth=0.1, label='square domain', zorder=5)
+    ax2.plot(nss, mtss/tss, 'b-', linewidth=0.1, label='square execution', zorder=5)
     ax2.plot(nss, mtss/tss, 'bo', markersize=2.5, markeredgecolor='b', zorder=5)
     ax2.plot([0,nss[-1]+1], [0, nss[-1]+1], 'k:', linewidth=0.1, label='ideal speedup', zorder=5)
     ax2.grid(color='lightgrey',linestyle='solid', linewidth=0.1 , zorder=1)
     ax2.legend(loc=0, prop={'size':9,'family':'serif'})
     ax2.set_title("Strong Scaling", size=13, family='serif')
-    ax2.set_xlabel("P [1]", family='serif')
-    ax2.set_ylabel(r"$t_{1}$/$t_{P} [1]$", family='serif')
+    ax2.set_xlabel(r"$P$  $[1]$", family='serif')
+    ax2.set_ylabel(r"Speedup  $t_{1}$/$t_{P}$  $[1]$", family='serif')
 
     
-    plt.subplots_adjust(left=0.08, right=0.95, top=0.85, bottom=0.1,wspace=0.2)
+    plt.subplots_adjust(left=0.07, right=0.97, top=0.9, bottom=0.2,wspace=0.2)
 
     # saving figures
     fig_path = workdir+'/'+'strong_scaling'+'.pdf'
